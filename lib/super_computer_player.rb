72a214f34fe4c9f827c72771e56c7190cee6623e
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-12 11:25:04 +0700

    finished tic tac toe

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,21 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    ttt_node = TicTacToeNode.new(game.board, mark)
+
+    ttt_node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    ttt_node.children.each do |child|
+      if !child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    raise "non losing node not found"
   end
 end
 
